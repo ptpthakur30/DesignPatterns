62e99f5a0ef5e4d8b4b8f53ff67cdb11f2df12b2
ptpthakur30 <ptpthakur30@gmail.com>
2020-06-10 15:33:46 +0530

    latest commit for behavioural pattern

---
+++ DesignPatterns_Behavioural_Iterator/ArrayBackedProperties.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using static System.Console;
+
+namespace DesignPatterns_Behavioural_Iterator
+{
+    public class Creature : IEnumerable<int>
+    {
+        //Array backded
+        private int[] stats = new int[3];
+
+        private const int strength = 0;
+
+        public int Strength
+        {
+            get => stats[strength];
+            set => stats[strength] = value;
+        }
+
+        public int Agility { get; set; }
+        public int Intelligence { get; set; }
+
+        //We can use array functions 
+        public double AverageStat =>
+          stats.Average();
+
+        //Can provide array as IEnumerable
+        public IEnumerator<int> GetEnumerator()
+        {
+            return stats.AsEnumerable().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int this[int index]
+        {
+            get { return stats[index]; }
+            set { stats[index] = value; }
+        }
+    }
+
+    public class ArrayBackedProperties
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
