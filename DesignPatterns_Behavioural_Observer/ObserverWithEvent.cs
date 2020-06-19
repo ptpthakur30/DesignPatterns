5b9b2ddaf407de1cf1c5a95e69579330095f14da
ptpthakur30 <ptpthakur30@gmail.com>
2020-06-19 23:02:52 +0530

    Behavioural pattern

--- DesignPatterns_Behavioural_Observer/ObserverWithEvent.cs
+++ DesignPatterns_Behavioural_Observer/ObserverWithEvent.cs
@@ -1,42 +1,42 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace DesignPatterns_Behavioural_Observer
-{
-    public class FallsIllEventArgs
-    {
-        public string Address;
-    }
+//namespace DesignPatterns_Behavioural_Observer
+//{
+//    public class FallsIllEventArgs
+//    {
+//        public string Address;
+//    }
 
-    public class Person
-    {
-        public void CatchACold()
-        {
-            FallsIll?.Invoke(this,
-              new FallsIllEventArgs { Address = "123 London Road" });
-        }
+//    public class Person
+//    {
+//        public void CatchACold()
+//        {
+//            FallsIll?.Invoke(this,
+//              new FallsIllEventArgs { Address = "123 London Road" });
+//        }
 
-        public event EventHandler<FallsIllEventArgs> FallsIll;
-    }
+//        public event EventHandler<FallsIllEventArgs> FallsIll;
+//    }
 
-    public class Demo
-    {
-        static void Main()
-        {
-            var person = new Person();
-            //Subscribe an event
-            person.FallsIll += CallDoctor;
+//    public class Demo
+//    {
+//        static void Main()
+//        {
+//            var person = new Person();
+//            //Subscribe an event
+//            person.FallsIll += CallDoctor;
 
-            //fire an event
-            person.CatchACold();
-        }
+//            //fire an event
+//            person.CatchACold();
+//        }
 
-        private static void CallDoctor(object sender, FallsIllEventArgs eventArgs)
-        {
-            Console.WriteLine($"A doctor has been called to {eventArgs.Address}");
-        }
-    }
-}
+//        private static void CallDoctor(object sender, FallsIllEventArgs eventArgs)
+//        {
+//            Console.WriteLine($"A doctor has been called to {eventArgs.Address}");
+//        }
+//    }
+//}
---
+++ DesignPatterns_Behavioural_Observer/ObserverWithSpecialInterface.cs
@@ -0,0 +1,85 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Reactive.Linq;
+//using static System.Console;
+
+//namespace DesignPatterns_Behavioural_Observer
+//{
+//    public class Event
+//    {
+
+//    }
+
+//    public class FallsIllEvent : Event
+//    {
+//        public string Address;
+//    }
+
+//    public class Person : IObservable<Event>
+//    {
+//        private readonly HashSet<Subscription> subscriptions
+//          = new HashSet<Subscription>();
+
+//        public IDisposable Subscribe(IObserver<Event> observer)
+//        {
+//            var subscription = new Subscription(this, observer);
+//            subscriptions.Add(subscription);
+//            return subscription;
+//        }
+
+//        //Used to free an event
+//        public void CatchACold()
+//        {
+//            foreach (var sub in subscriptions)
+//                sub.Observer.OnNext(new FallsIllEvent { Address = "123 London Road" });
+//        }
+
+//        //Used to store observable and observer
+//        private class Subscription : IDisposable
+//        {
+//            private Person person;
+//            public IObserver<Event> Observer;
+
+//            public Subscription(Person person, IObserver<Event> observer)
+//            {
+//                this.person = person;
+//                Observer = observer;
+//            }
+
+//            public void Dispose()
+//            {
+//                person.subscriptions.Remove(this);
+//            }
+//        }
+//    }
+
+//    public class Demo : IObserver<Event>
+//    {
+//        static void Main(string[] args)
+//        {
+//            new Demo();
+//        }
+
+//        public Demo()
+//        {
+//            var person = new Person();
+//            //var sub = person.Subscribe(this);
+
+//            //If FallsIllEvent type of event occurs then what to do is given by subscribe
+//            //Event contains arguments
+//            person.OfType<FallsIllEvent>()
+//              .Subscribe(args => WriteLine($"A doctor has been called to {args.Address}"));
+          
+//            person.CatchACold();
+//        }
+
+//        public void OnNext(Event value)
+//        {
+//            if (value is FallsIllEvent args)
+//                WriteLine($"A doctor has been called to {args.Address}");
+//        }
+
+//        public void OnError(Exception error) { }
+//        public void OnCompleted() { }
+//    }
+//}
